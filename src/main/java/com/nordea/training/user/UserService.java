4649784fecd518196681ef38b138be8c58b3a35c
Mariusz Strzelecki <szczeles@gmail.com>
2018-11-23 16:01:30 +0100

    fix

--- src/main/java/com/nordea/training/user/UserService.java
+++ src/main/java/com/nordea/training/user/UserService.java
@@ -1,7 +1,9 @@
 package com.nordea.training.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class UserService {
     public static void main(String[] args) {
         System.setProperty("server.port", "9000");
